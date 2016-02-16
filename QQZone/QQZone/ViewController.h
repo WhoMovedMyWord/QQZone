c4c357d860fdb26702d35211b0f770cff06f89f5
kevin <920523446@qq.com>
2016-02-16 23:34:04 +0800

    init-初始化项目

---
+++ QQZone/QQZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQZone
+//
+//  Created by app;e on 16/2/16.
+//  Copyright © 2016年 world. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQZone/QQZone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQZone
+//
+//  Created by app;e on 16/2/16.
+//  Copyright © 2016年 world. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
