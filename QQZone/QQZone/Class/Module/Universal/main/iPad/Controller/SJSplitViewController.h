1345986c24707f09ba6b0cbf29d78999ce9397e6
kevin <920523446@qq.com>
2016-02-17 11:00:14 +0800

    项目模块划分

---
+++ QQZone/QQZone/Class/Module/Universal/main/iPad/Controller/SJSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SJSplitViewController.h
+//  QQZone
+//
+//  Created by app;e on 16/2/17.
+//  Copyright © 2016年 world. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJSplitViewController : UISplitViewController
+
+@end
---
+++ QQZone/QQZone/Class/Module/Universal/main/iPad/Controller/SJSplitViewController.m
@@ -0,0 +1,31 @@
+//
+//  SJSplitViewController.m
+//  QQZone
+//
+//  Created by app;e on 16/2/17.
+//  Copyright © 2016年 world. All rights reserved.
+//
+
+#import "SJSplitViewController.h"
+
+@interface SJSplitViewController ()
+
+@end
+
+@implementation SJSplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self prepareSplitVC];
+}
+
+- (void)prepareSplitVC{
+    //1.设置master 与 deatal 的显示样式
+    
+    //2.设置masterVC的宽度
+    //2.1获取横竖屏幕
+}
+
+@end
