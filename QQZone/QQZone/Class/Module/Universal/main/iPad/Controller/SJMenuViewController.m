12ebbd6e1391548ef5b3c43c28dcd0f1de8ad918
kevin <920523446@qq.com>
2016-02-18 16:01:58 +0800

    ipad的masterVC底部stackView设置

--- QQZone/QQZone/Class/Module/Universal/main/iPad/Controller/SJMenuViewController.m
+++ QQZone/QQZone/Class/Module/Universal/main/iPad/Controller/SJMenuViewController.m
@@ -12,16 +12,23 @@
 //容器视图,显示的是iphone的tabBar界面,只有当spliteVC的宽度是紧凑的时候才显示
 @property (weak, nonatomic) IBOutlet UIView *container;
 
+//masterVC最底部的stackView
+@property(nonatomic,strong) UIStackView *bottomStackView;
+
 @end
 
 @implementation SJMenuViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    //容器view
     [self setupContainerView];
+    //master底部的stackView
+    [self prepareBottomStackView];
 }
-
+/**
+ *  准备容器view
+ */
 - (void)setupContainerView{
     //1.创建iPhone的tabBarVC
     SJMainTableViewController *tabBarVC = [[SJMainTableViewController alloc] init];
@@ -35,6 +42,51 @@
 }
 
 /**
+ *  准备masterVC底部的stackView
+ */
+- (void)prepareBottomStackView{
+    //1.添加stackView
+    [self.view addSubview:self.bottomStackView];
+    self.bottomStackView.backgroundColor = [UIColor orangeColor];
+    
+    self.bottomStackView.axis = UILayoutConstraintAxisHorizontal;//默认
+    self.bottomStackView.alignment = UIStackViewAlignmentFill;//默认.垂直方向填充stackview
+    self.bottomStackView.distribution = UIStackViewDistributionFillEqually;//水平方向等宽
+    self.bottomStackView.spacing = 5;//间距
+    
+    /*stackView的排列方式
+     @property(nonatomic) UILayoutConstraintAxis axis;//子控件排列方式:水平or垂直
+     
+    @property(nonatomic) UIStackViewDistribution distribution;//子控件水平方向排列方式
+    
+     @property(nonatomic) UIStackViewAlignment alignment; //子控件垂直方向排列方式
+    
+    @property(nonatomic) CGFloat spacing; //间距
+
+     */
+    
+    //2.设置stackView的frame
+    [self.bottomStackView mas_makeConstraints:^(MASConstraintMaker *make) {
+        //stackView的右边与当前view的右边相等. 底部,左侧一样
+        make.trailing.leading.bottom.equalTo(self.view);
+        make.height.equalTo(@90);
+    }];
+    
+    //3.准备数据
+    NSArray *titleArray = @[@{@"title":@"发说说"},
+                            @{@"title":@"传照片"},
+                            @{@"title":@"写日志"}];
+    
+    //4.创建子控件
+    for (NSDictionary *dic in titleArray) {
+        UIButton *btn = [[UIButton alloc] init];
+        [btn setTitle:dic[@"title"] forState:UIControlStateNormal];
+        [self.bottomStackView addArrangedSubview:btn];
+    }
+    
+}
+
+/**
  *  容器视图是否显示
  *
  *  @param isShow <#isShow description#>
@@ -42,4 +94,15 @@
 - (void)showContainer:(BOOL)isShow{
     self.container.hidden = !isShow;
 }
+
+#pragma mark - 懒加载
+- (UIStackView *)bottomStackView{
+    
+    if (_bottomStackView == nil) {
+        _bottomStackView = [[UIStackView alloc] init];
+    }
+    
+    return _bottomStackView;
+}
+
 @end
